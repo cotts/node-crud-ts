1912592e908f6136914f76d5e44bea55e37bb7a9
Thadeu Cotts <thadeu.cotts@datopian.com>
2021-03-14 20:41:05 -0300

    [fetch] : add fetch all books

---
+++ src/api/v1/book/book.service.ts
@@ -0,0 +1,31 @@
+import Book, { IBook } from './book.model'
+import mongoose from 'mongoose'
+
+interface IParams {
+  skip: number
+  limit: number
+}
+
+/**
+ * Format Query params to fetch book
+ * @param skip - skip value
+ * @param limit - limit value
+ */
+const parseQuery = (skip: string, limit: string): IParams => ({
+  skip: parseInt(skip) || 0,
+  limit: parseInt(limit) || 50,
+})
+
+/**
+ *
+ * @param skip
+ * @param limit
+ */
+export const fetchAll = async (
+  skipParam: string,
+  limitParam: string
+): Promise<IBook[]> => {
+  const { skip, limit } = parseQuery(skipParam, limitParam)
+
+  return Book.find().skip(skip).limit(limit)
+}

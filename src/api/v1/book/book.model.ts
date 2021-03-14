e8a5725fd4afa39d240e5cdd742b1c21e1bad437
Thadeu Cotts <thadeu.cotts@datopian.com>
2021-03-14 20:40:45 -0300

    [fix] : update model interface and exports

--- src/api/v1/book/book.model.ts
+++ src/api/v1/book/book.model.ts
@@ -1,31 +1,29 @@
 import mongoose, { Schema, Document, Model, model } from 'mongoose'
 
-export interface IBook extends Document{
-  title: String,
-  description: String,
-  author: String,
-  edition: Number
+export interface IBook extends Document {
+  title: string
+  description: string
+  author: string
+  edition: number
 }
 
 const BookSchema: Schema = new Schema({
   title: {
     type: String,
-    required: true
+    required: true,
   },
-   description: {
+  description: {
     type: String,
-    required: true
-  }, 
+    required: true,
+  },
   author: {
     type: String,
-    required: true
-  }, 
+    required: true,
+  },
   edition: {
     type: Number,
-    required: true
-  }
+    required: true,
+  },
 })
 
-const Book: Model<IBook> = model('Book', BookSchema) 
-
-export default Book
+export default mongoose.model<IBook>('Book', BookSchema)

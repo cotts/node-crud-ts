5b0706f603575049ee9523619325d794733b7428
Thadeu Cotts <thadeu.cotts@datopian.com>
2021-03-15 11:37:20 -0300

    [app] : import modules from api

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import dotenv from 'dotenv'
-import express from 'express'
+import express, { Router } from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
+import api from './api'
 
 dotenv.config()
 
+const router = Router().use('/api', api)
 export default express()
   .use(cors())
   .use(helmet())
   .use(express.json())
+  .use(router)

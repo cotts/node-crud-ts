7334df47524630f23a70e3c58c24deebbfdc6619
Thadeu Cotts <thadeu.cotts@datopian.com>
2021-03-15 11:53:30 -0300

    [controller] : add request interface and controller to create

--- src/api/v1/book/book.controller.ts
+++ src/api/v1/book/book.controller.ts
@@ -1,19 +1,34 @@
-import { request, Request, Response } from 'express'
+import { request, Response } from 'express'
 import { IBook } from './book.model'
 import * as service from './book.service'
 
+interface Params {
+  skip: string
+  limit: string
+}
+
+interface Request {
+  query: {
+    skip: string
+    limit: string
+  }
+  params: {
+    id: string
+  }
+  body: IBook
+}
 /**
  * Get All book
  * @param req Request from API
  * @param res Response to API
  */
 export const fetchAll = (req: Request, res: Response): void => {
-  const { skip, limit } = req.query
+  const { skip, limit }: Params = req.query
 
   service
     .fetchAll(skip, limit)
     .then((data) => res.json(data))
-    .catch((error) => res.send(error.message))
+    .catch((error) => res.json({ message: error.message }))
 }
 
 /**
@@ -25,6 +40,15 @@ export const getById = (req: Request, res: Response): void => {
   const id = parseInt(req.params.id)
   service
     .getById(id)
+    .then((data) => res.json(data))
+    .catch((error) => res.send(error.message))
+}
+
+export const create = (req: Request, res: Response): void => {
+  const book: IBook = req.body
+
+  service
+    .create(book)
     .then((data) => res.json(data))
     .catch((error) => res.send(error.message))
 }

cd9601b5480dff154b0c5f6e27a192ccb09f9973
Thadeu Cotts <thadeu.cotts@datopian.com>
2021-03-14 23:54:05 -0300

    [database] : init database

--- bin/main.ts
+++ bin/main.ts
@@ -1,12 +1,11 @@
 import dotenv from 'dotenv'
 import app from '../src'
+import * as databse from '../src/lib/database'
 
 dotenv.config()
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.info(`Server Running on Port ${PORT}`)
-  
+  databse.connect()
 })
-
-
